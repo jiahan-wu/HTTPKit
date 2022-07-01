8dcbe12debb99df74f6f865558136877c9af17fa
Jia-Han Wu <jiahan.wu@icloud.com>
2022-07-01 20:57:29 +0800

    Initial import

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "HTTPKit",
+    products: [
+        .library(name: "HTTPKit", targets: ["HTTPKit"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "HTTPKit", dependencies: []),
+        .testTarget(name: "HTTPKitTests", dependencies: ["HTTPKit"]),
+    ]
+)
---
+++ Sources/HTTPKit/HTTPKit.swift
@@ -0,0 +1,6 @@
+public struct HTTPKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
